1a98afca5dfb2ead7a95a71dab02cd95119a72ea
Paranoid Wolf <ajuph9224@gmail.com>
2022-10-04 10:42:19 +0000

    added template gohtml file

--- storage/storage.go
+++ storage/storage.go
@@ -2,16 +2,18 @@ package storage
 
 import (
 	"boarder/models"
+	"fmt"
+	"html/template"
 	"os"
 	"strings"
 )
 
 /*
-    Store data of posts in a md file on the correct folder
+   Store data of posts in a md file on the correct folder
 
-    @param (string, []Posts) - (details of board and thread, array of posts in thread)
+   @param (string, []Posts) - (details of board and thread, array of posts in thread)
 
-    @return error - error from creation of file
+   @return error - error from creation of file
 */
 func Store_posts_in_md(entry string, posts []models.Post) error {
     content := models.Format_posts_to_string(posts)
@@ -25,5 +27,36 @@ func Store_posts_in_md(entry string, posts []models.Post) error {
     }
     defer f.Close()
     f.WriteString(content)
+    Create_html_page(entry, posts)
+    
+    return nil
+}
+
+func Create_html_page(entry string, posts []models.Post) error {
+    t := template.Must(template.ParseFiles("./storage/template.gohtml"))
+    entry_elements := strings.Split(entry, "_")
+    board := entry_elements[0]
+    thread := entry_elements[1]
+
+    data := struct {
+        Title string
+        Posts []models.Post
+    }{
+        Title: entry,
+        Posts: posts,
+    }
+
+    file_name := "archive" + string(os.PathSeparator) + board + string(os.PathSeparator) + thread + string(os.PathSeparator) + "index.html"
+    f, err := os.Create(file_name)
+    if err != nil {
+        return err
+    }
+    defer f.Close()
+
+    err = t.Execute(f, data)
+    if err != nil {
+        return err
+    }
+
     return nil
 }
